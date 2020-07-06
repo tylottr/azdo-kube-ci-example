f3a8bce702db53c4c0bb228413c97d7f443e7983
Ty <tyla.boff@outlook.com>
2020-07-06 17:33:45 +0100

    updated imports

--- application/flask/app.py
+++ application/flask/app.py
@@ -1,20 +1,19 @@
 # Load application dependencies
+import os, json
 from flask import Flask
-from os import environ
-from json import dumps
 app = Flask(__name__)
 
 # Configure application
-HOST = environ.get("ADDRESS") or "0.0.0.0"
-PORT = environ.get("PORT") or 3000
+HOST = os.environ.get("ADDRESS") or "0.0.0.0"
+PORT = os.environ.get("PORT") or 3000
 
 # Configure env vars
-APP_NAME = environ.get("APP_NAME") or "unnamed"
+APP_NAME = os.environ.get("APP_NAME") or "unnamed"
 
 # Routes
 @app.route("/")
 def index():
-    return dumps({
+    return json.dumps({
       "sourceRepo": "kube-ci-example",
       "language": "python",
       "appName": APP_NAME
